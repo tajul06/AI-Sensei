1023cf1da8a780ad2de6b0abe695165e7c234d07
tajul06 <md.tajul2000@gmail.com>
2026-08-14 00:56:02 +0600

    AI Sensei Initiate

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -6,4 +6,10 @@ from tqdm import tqdm
 from pinecone import Pinecone ,ServerlessSpec
 from langchain_community.document_loaders import pypdfloader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+load_dotenv()
+hf_api_key = os.environ.get("HF_API_KEY")
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+
