b6aa8a4c7cda652d0fd9a5757fed834b49fa08c0
Md. Tajul Islam <88366976+tajul06@users.noreply.github.com>
2026-08-27 07:05:33 +0600

    Delete client directory
    removing medibot frontend

--- client/components/history_dload.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def render_history_download_ui():
-    if st.session_state.get("chat_history"):
-        st.header("Download Chat History")
-        chat_history = "\n".join([f"{msg['role'].capitalize()}: {msg['content']}" for msg in st.session_state.chat_history])
-        st.download_button("Download Chat History", chat_history, file_name="chat_history.txt", mime="text/plain")
-
-        
